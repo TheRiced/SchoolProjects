dabe89170a18714df495f082c6920004124f4f9c
TheRiced <raisar@stud.ntnu.no>
2025-03-18 22:38:20 +0100

    feat(CardContainer): add interface
    Interface with methods:
    -getCardCount(): returns the number of cards.
    -getCard(int index): returns the card at the specified index
    -getCards(): returns a list of all cards.
    
    Constructor to initialize the deck with a full set of cards
    Added dealHand(int n) method to deal a specified number and shuffled

---
+++ src/main/java/ntnu/idatt2003/CardDeck.java
@@ -0,0 +1,43 @@
+package ntnu.idatt2003;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+public class CardDeck implements CardContainer{
+  private final List<Card> cards = new ArrayList<>();
+
+  public CardDeck() {
+    for (char suit : Card.suits) {
+      for (int face = 1; face <= 13; face++) {
+        cards.add(new Card(suit, face));
+      }
+    }
+  }
+
+  public List<Card> dealHand(int n) {
+    if (n < 1 || n > cards.size()) {
+      throw new IllegalArgumentException("Invalid number of cards to deal");
+    }
+    Collections.shuffle(cards);
+    return new ArrayList<>(cards.subList(0, n));
+  }
+
+  @Override
+  public int getCardCount() {
+    return cards.size();
+  }
+
+  @Override
+  public Card getCard(int index) {
+    if (index < 0 || index >= cards.size()) {
+      throw new IllegalArgumentException("Invalid index");
+    }
+    return cards.get(index);
+  }
+
+  @Override
+  public List<Card> getCards() {
+    return new ArrayList<>(cards);
+  }
+}
