d6b831350974b059edaa77960be3933fdad8439a
TheRiced <raisar@stud.ntnu.no>
2025-03-19 22:55:55 +0100

    feat(card-container): add JavaDoc comments for better documentation

--- src/main/java/ntnu/idatt2003/CardContainer.java
+++ src/main/java/ntnu/idatt2003/CardContainer.java
@@ -2,6 +2,10 @@ package ntnu.idatt2003;
 
 import java.util.List;
 
+/**
+ *A container that holds Card objects
+ */
+
 public interface CardContainer {
     int getCardCount();
     Card getCard(int index);
