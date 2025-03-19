d1b38184ace0d7efa43a9a4b2b16de254c0d301f
TheRiced <raisar@stud.ntnu.no>
2025-03-19 23:21:04 +0100

    feat: add javadoc

--- src/main/java/ntnu/idatt2003/CardContainer.java
+++ src/main/java/ntnu/idatt2003/CardContainer.java
@@ -7,7 +7,20 @@ import java.util.List;
  */
 
 public interface CardContainer {
+    /**
+     * retives the count of cards
+     * @return the number of cards in the container
+     */
     int getCardCount();
+    /**
+     * retrieves a card from the container
+     * @param index the index of the card to retrieve
+     * @return the card at the given index
+     */
     Card getCard(int index);
+    /**
+     * retrieves a hand of cards from the container
+     * @return a list of cards representing the hand
+     */
     List<Card> getCards();
 }
